fc1e4dbe60cc3d10306b06930009b4da3cef28c0
maletosik <aleksandr.maletin.2000@gmail.com>
2023-10-19 11:50:00 +0700

    user

---
+++ user/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+if(username.ToLower() == "maletos")
+{
+    Console.WriteLine("приветствую, мой Господин");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
